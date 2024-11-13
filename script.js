ef71cd31f8cc9e9d7fe4da956588d87ae7d14822
Suryansh064 <singh.suryansh064@gmail.com>
2024-11-13 18:19:17 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("b11").onclick = function() {
+    document.body.style.backgroundColor = "rgb(0, 0, 255)";
+};
+
+document.getElementById("b12").onclick = function() {
+    document.body.style.backgroundColor = "red";
+};
+
+document.getElementById("b13").onclick = function() {
+    document.body.style.backgroundColor = "yellow";
+};
+
+document.getElementById("b14").onclick = function() {
+    document.body.style.backgroundColor = "green";
+};
+
